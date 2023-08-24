85158321b9a822cf22fcd5d070abfa2c4bbad0a5
Douglas Nadoti <dodo_nadoti@hotmail.com>
2023-08-23 23:59:39 -0300

    cart implementacao

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { useEffect } from "react"
+import Image from "next/image"
+import { Fragment, useEffect } from "react"
 
 const arrayPokemon = [
     {
@@ -13,7 +14,8 @@ const arrayPokemon = [
           name: "poison"
         },
       ],
-      generation: "generation-i"
+      generation: "generation-i",
+      color: "green",
     },
     {
         id: 1,
@@ -26,7 +28,8 @@ const arrayPokemon = [
             name: "poison"
           },
         ],
-        generation: "generation-i"
+        generation: "generation-i",
+        color: "green",
       },
       {
         id: 1,
@@ -39,7 +42,8 @@ const arrayPokemon = [
             name: "poison"
           },
         ],
-        generation: "generation-i"
+        generation: "generation-i",
+        color: "green",
       },
     {
         id: 1,
@@ -52,7 +56,8 @@ const arrayPokemon = [
             name: "poison"
             },
         ],
-        generation: "generation-i"
+        generation: "generation-i",
+        color: "green",
     },
   ]
 
@@ -63,9 +68,52 @@ export function Cart() {
     }, [])
 
     return (
-        <section className="block absolute inset-0 w-full min-h-screen bg-slate-950 bg-opacity-50 no-scrollbar-container overflow-hidden">
-            <aside className="w-full max-w-lg h-full bg-white overflow-hidden ">
-                
+        <section className="block absolute inset-0 bg-slate-950 bg-opacity-50">
+            <aside className="w-full relative max-w-lg h-full z-10 bg-white">
+                <button className="w-12 h-12 absolute top-0 right-0 border-2 border-red-500 text-red-500 font-bold rounded-s-md transition-all hover:text-white hover:bg-red-500">X</button>
+                <div className="h-full flex flex-col justify-between">
+                    <div className="text-center pt-10 mb-5">
+                        <h1 className="text-3xl text-gray-700">Lista de Pokemons</h1>
+                    </div>
+                    <div className="flex flex-col gap-5 overflow-y-scroll flex-1 px-2">
+                        {arrayPokemon.map((pokemon, index) => (
+                            <div 
+                                key={index}
+                                className={`border rounded-lg flex items-center justify-between px-4`}
+                                style={{background: pokemon.color}}
+                            >
+                                <div className="relative w-20 h-20 max-w-full">
+                                    <Image
+                                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+                                        alt={pokemon.name}
+                                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                        className="w-full mx-auto flex items-center justify-center"
+                                        loading="lazy"
+                                        priority={false}
+                                        placeholder="empty"
+                                        fill
+                                    />
+                                </div>
+                                <div>
+                                    <h2 className=" text-xl">{pokemon.name}</h2>
+                                </div>
+                                <ul>
+                                    {pokemon.type.map((type, i) => (
+                                        <li key={i}>{type.name}</li>
+                                    ))}
+                                </ul>
+                                <div className="">
+                                    <p>{pokemon.generation}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="px-2 mt-5 pb-5">
+                        <button className="w-full bg-gray-500 text-white py-4 font-bold rounded-md">
+                            Capture
+                        </button>
+                    </div>
+                </div>
             </aside>
         </section>
     )
